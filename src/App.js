908ebd485fd946bf3566533c3a17901a563b1c19
Lee <lee.neufeld@ganjagurus.ca>
2021-12-02 12:59:01 -0700

    Feat: started home and footer

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import './App.css';
+import "./App.css";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from './pages/home';
-import Contact from './pages/contact';
-import FarmStore from './pages/farmStore';
-import PriceList from './pages/priceList';
-import NavBar from './components/navBar';
+import Home from "./pages/home";
+import Contact from "./pages/contact";
+import FarmStore from "./pages/farmStore";
+import PriceList from "./pages/priceList";
+import NavBar from "./components/navBar";
+import Footer from "./components/footer";
 
 function App() {
   return (
     <div className="page-container">
-      
-        <NavBar/>
-        <Router>
-          <Routes>
-          <Route exact path="/" element={<Home/>} />
-          <Route exact path="/farmstore" element={<FarmStore/>} />
-          <Route exact path="/contact" element={<Contact/>} />
-          <Route exact path="/pricelist" element={<PriceList/>} />
-          </Routes>
-        </Router>
-        
-      
+      <NavBar />
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/farmstore" element={<FarmStore />} />
+          <Route exact path="/contact" element={<Contact />} />
+          <Route exact path="/pricelist" element={<PriceList />} />
+        </Routes>
+      </Router>
+      <Footer />
     </div>
   );
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,15 +1,69 @@
-import { Container, Grid } from "@mui/material";
+import { Container, Grid, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 import React from "react";
+import FarmSign from "../images/farmSign-removebg.png";
+import SheepSunset from "../images/sheep2-min.jpg";
+import { styled } from "@mui/material/styles";
 
+import Paper from "@mui/material/Paper";
+
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
 
 function Home() {
   return (
-    <div >
-      <Container>
-        <Grid>
-          
+    <div>
+      <Box>
+        <Grid container textAlign="center" spacing={2}>
+          <Grid sx={{ display: { xs: "none", lg: "block" } }} lg={2} />
+          <Grid item xs={12} lg={8} sx={{ m: 3 }}>
+            <img
+              width="50%"
+              height="auto"
+              className=" img-fluid "
+              alt="Responsive"
+              src={FarmSign}
+            />
+          </Grid>
+          <Grid sx={{ display: { xs: "none", lg: "block" } }} lg={2} />
+          <Grid justifyItems="center" xs={12}>
+            <Typography variant="h3" sx={{ m: 3 }} gutterBottom component="div">
+              Welcome to Hidden Creek Farm
+            </Typography>
+          </Grid>
+          <Grid sx={{ display: { xs: "none", lg: "block" } }} lg={4} />
+          <Grid justifyItems="center" xs={12} lg={4}>
+            <Typography
+              variant="h6"
+              sx={{ width: "100%", maxWidth: 700 }}
+              sx={{ m: 3 }}
+              component="div"
+            >
+              Taste that old fashioned wholesome goodness with our Hidden Creek
+              Lamb. Raised naturally on the lush grasses of the Pacific
+              Northwest. Our ewes are bred for meat and not wool, therefore give
+              a far more superior flavor than the imported commercial cuts you
+              find in stores.
+            </Typography>
+          </Grid>
+          <Grid sx={{ display: { xs: "none", lg: "block" } }} lg={4} />
+          <Grid sx={{ display: { xs: "none", lg: "block" } }} lg={2} />
+          <Grid item xs={12} lg={8} sx={{ m: 3 }}>
+            <img
+              width="100%"
+              height="auto"
+              className=" img-fluid "
+              alt="Responsive"
+              src={SheepSunset}
+            />
+          </Grid>
+          <Grid sx={{ display: { xs: "none", lg: "block" } }} lg={2} />
         </Grid>
-      </Container>
+      </Box>
     </div>
   );
 }
